cf5796e26cd2265248ae8f50c9d840a2791f1f3c
Traci7822 <Tracir@gmail.com>
2016-10-26 18:40:07 -0600

    added header to sequences index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.authenticate(params[:username], params[:password])
+    user = User.find_or_create_by(username: params[:username])
     if user
-      session[:user_id] = params[:user_id]
-      redirect_to root_path, :notice => "You are logged in."
+      session[:user_id] = user.id
+      redirect_to root_path
+      #:notice => "You are logged in."
     else
-      flash.now.alert = "Invalid username or password"
+      #flash.now.alert = "Invalid username or password"
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/', :notice => "You are logged out."
+    redirect_to '/'
+    # :notice => "You are logged out."
   end
 end
