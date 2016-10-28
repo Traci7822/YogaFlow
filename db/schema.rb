92c505e021d17d59caca6ac7aac613e21f20f018
Traci7822 <Tracir@gmail.com>
2016-10-28 12:07:12 -0600

    added omniauth columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028081314) do
+ActiveRecord::Schema.define(version: 20161028180521) do
 
   create_table "poses", force: :cascade do |t|
     t.string   "name"
