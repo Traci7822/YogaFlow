9dd0f68c9ca415cddaf8d0236d67e1efb7130e33
Traci7822 <Tracir@gmail.com>
2016-10-31 14:42:44 -0600

    set and formatted comments on sequences

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(content: params[:comment][:content], sequence_id: params[:sequence_id], user_id: current_user.id)
+    redirect_to sequence_poses_path
+  end
+
 end
