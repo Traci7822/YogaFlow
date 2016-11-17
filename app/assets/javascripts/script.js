e8d34c7e86584d1cc9851d1b76de93c6f12bf8d5
Traci7822 <Tracir@gmail.com>
2016-11-17 15:10:59 -0700

    collecting data from serializer correctly

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
 })
 
 function listPoses() {
+  debugger;
   var id = parseInt(window.location.pathname.split("/")[2]);
   $.get('/sequences/' + id + '/list', function(data) {
+    debugger;
     $("#poses").text(data);
   })
 }
