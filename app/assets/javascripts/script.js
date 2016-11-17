16bb80119f35074739ea7b73bee28bdad417aae4
Traci7822 <Tracir@gmail.com>
2016-11-17 15:46:18 -0700

    added showSequence to populate sequence info and moved poses into own method

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-  listPoses();
+  showSequence();
 })
 
-function listPoses() {
-  debugger;
+
+function showSequence() {
   var id = parseInt(window.location.pathname.split("/")[2]);
   $.get('/sequences/' + id + '/list', function(data) {
-    debugger;
-    $("#poses").text(data);
+    $("#sequence-title").html('"' + data.title + '"')
+    $("#sequence-difficulty").html("Difficulty Rating: " + data.difficulty)
+    $("#repititions").html("For " + data.repititions + " rounds, repeat the following sequence of poses:")
+    listPoses(data);
   })
-}
+  };
+
+function listPoses(data){
+  debugger;
+  poses = data.poses
+  for (var i = 0; i < poses.length; i++){
+    $("#poses").append('<li>' + poses[i].name + '</li>');
+  }
+};
